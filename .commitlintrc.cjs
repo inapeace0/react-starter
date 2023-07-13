389e7491c8af4806e6e6ddaf6fa2c357d952c6bb
cacablocker <dynamiclikeyou@gmail.com>
2023-07-13 16:33:37 +0900

    chore(commitlint): add a new category - "deps"
    add a new category - "deps" - update dependencies

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,10 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        "type-enum": [
+          2,
+          "always",
+          ["feat", "fix", "chore", "docs", "style", "refactor", "perf", "test", "deps"]
+        ]
+    }
 }
